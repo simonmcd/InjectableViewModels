c28b7895cbcb15e98c0991696939f80478e6aa2c
Simon McDonnell <simonmcdgit@gmail.com>
2019-06-22 22:22:12 +0100

    Add ability to specify view model scope to fragment or parent activity

--- libraryfragments/src/main/java/com/simonmcd/injectableviewmodels/fragments/ViewModelFragment.kt
+++ libraryfragments/src/main/java/com/simonmcd/injectableviewmodels/fragments/ViewModelFragment.kt
@@ -12,21 +12,40 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import javax.inject.Inject
 
-
+/**
+ * A Fragment that will use the [VIEW_BINDING] specified for its UI and will inject the [VM] specified. The [VM] must be
+ * provided through the injected [ViewModelProvider.Factory].
+ */
 abstract class ViewModelFragment<VIEW_BINDING : ViewDataBinding, VM : ViewModel> : Fragment() {
 
     lateinit var binding: VIEW_BINDING
 
+    /**
+     * The resource ID for this Fragment.
+     */
     abstract val layoutResourceID: Int
 
+    /**
+     * The class of ViewModel to be injected.
+     */
     abstract val viewModelClass: Class<VM>
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
     val viewModel by lazy {
-        ViewModelProviders.of(this, viewModelFactory).get(viewModelClass)
+        val viewModelProvider = if (limitViewModelScopeToFragment) {
+            ViewModelProviders.of(this, viewModelFactory)
+        } else {
+            ViewModelProviders.of(requireActivity(), viewModelFactory)
+        }
+        viewModelProvider.get(viewModelClass)
     }
+    /**
+     * When true, the ViewModel provided will be tied to the Fragment. Otherwise it will be tied to the parent Activity.
+     * By sharing with the parent Activity multiple Fragments may share data using the same ViewModel instance.
+     */
+    protected open val limitViewModelScopeToFragment = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, layoutResourceID, container, false)
